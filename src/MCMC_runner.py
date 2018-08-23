19742d6624505597bd6ac86c4bf6cb086e9dac78
Ivan Peng <ivanpeng13@gmail.com>
2018-08-23 15:47:46 -0400

    Move files into src

--- src/MCMC_runner.py
+++ src/MCMC_runner.py
@@ -1,5 +1,10 @@
 import Algorithmia
 
+"""
+This python script will submit a PyMC3 MCMC job to Algorithmia and return the output. This assumes the schema is
+already correct. It will simply connect to the algorithm set up in Algorithmia and run the commands.
+"""
+
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
