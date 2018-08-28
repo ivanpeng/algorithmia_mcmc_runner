91ebca4b2be624a143786669584ad8a89960c66f
Ivan Peng <ivanpeng13@gmail.com>
2018-08-28 16:23:43 -0400

    Test case for deterministic equation

--- test/test_schema_validator.py
+++ test/test_schema_validator.py
@@ -69,7 +69,7 @@ class TestSchemaValidator(unittest.TestCase):
         validator = SchemaValidator(obj)
         self.assertRaises(ValidationError, validator._validate_dependencies)
 
-    def test_full_schema_with_non_existent_field_in_deterministic_fails(self):
+    def test_full_schema_with_nonexistent_field_in_prior_fails(self):
         with open("src/schema-template.json", "r") as f:
             obj = json.load(f)
         obj["schema"]["formulae"][0]["priors"].append({
@@ -85,5 +85,13 @@ class TestSchemaValidator(unittest.TestCase):
         with open("src/schema-template.json", "r") as f:
             obj = json.load(f)
         obj["schema"]["formulae"][0]["priors"][0]["type"] = "pareto"
+        validator = SchemaValidator(obj)
+        self.assertRaises(ValidationError, validator._validate_dependencies)
+
+    def test_full_schema_with_nonexistent_field_in_deterministic_eq_fails(self):
+        with open("src/schema-template.json", "r") as f:
+            obj = json.load(f)
+        obj["schema"]["formulae"][0]["deterministic"]["formula"] = "1.2 * returns_stock_1 + returns_bond_1 - " \
+                                                                   "a_nonexistent_prior "
         validator = SchemaValidator(obj)
         self.assertRaises(ValidationError, validator._validate_dependencies)
